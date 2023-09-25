ef294c421275525e7f91322064ee2f77621661ec
Solo <abdirashidsolo>
2023-09-25 17:47:51 +0300

    Stripe improvment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -58,7 +58,7 @@ exports.getBookingsSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ emai: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.unit_amount / 100;
   await Booking.create({ tour, user, price });
 };
 
