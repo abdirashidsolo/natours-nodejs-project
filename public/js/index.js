60103e98c610212b9c8b6f952daefbe50389cbf4
Solo <abdirashidsolo>
2023-09-26 11:33:07 +0300

    fixed the weird looking alert that apears our web

--- public/js/index.js
+++ public/js/index.js
@@ -76,7 +76,5 @@ if (bookBtn) {
   });
 }
 
-const alert = document.querySelector('body').dataset.alert;
-if (alert) {
-  showAlert('success', alert, 10);
-}
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
