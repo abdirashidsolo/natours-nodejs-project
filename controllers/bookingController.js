2587feab371f80a3f29b21a748de3c287272927e
Solo <abdirashidsolo>
2023-10-07 17:53:15 +0300

    fixed some problems

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -62,9 +62,9 @@ exports.getBookingsSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.unit_amount / 100;
+  const price = session.amount_total / 100;
 
-  console.log(tour, user, price);
+  // console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
